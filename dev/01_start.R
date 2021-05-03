e5936b4a533773a60e49144000b12c778a7fe76e
Chris Beeley <chris.beeley@gmail.com>
2021-05-03 09:44:21 +0100

    NHS-R favicon :rocket:

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,12 +20,14 @@
 ## 
 golem::fill_desc(
   pkg_name = "shinySPC", # The Name of the package containing the App 
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App 
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional) 
+  pkg_title = "A Shiny application to draw SPC charts", # The Title of the package containing the App 
+  pkg_description = "This application allows you to load data from a variety 
+  of popular formats and then plot SPC and run charts, all from a simple Shiny 
+  interface", # The Description of the package containing the App 
+  author_first_name = "Chris", # Your First Name
+  author_last_name = "Beeley", # Your Last Name
+  author_email = "chris.beeley@gmail.com", # Your Email
+  repo_url = "https://github.com/nhs-r-community/shinySPC" # The URL of the GitHub Repo (optional) 
 )     
 
 ## Set {golem} options ----
@@ -33,7 +35,7 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license( name = "Golem User" )  # You can set another license here
+usethis::use_mit_license(copyright_holder = "NHS-R community")
 usethis::use_readme_rmd( open = FALSE )
 usethis::use_code_of_conduct()
 usethis::use_lifecycle_badge( "Experimental" )
@@ -51,7 +53,7 @@ golem::use_recommended_deps()
 
 ## Favicon ----
 # If you want to change the favicon (default is golem's one)
-golem::use_favicon() # path = "path/to/ico". Can be an online file. 
+golem::use_favicon("inst/app/www/nhsr.png") # path = "path/to/ico". Can be an online file. 
 golem::remove_favicon()
 
 ## Add helper functions ----
