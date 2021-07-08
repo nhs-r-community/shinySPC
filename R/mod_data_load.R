88fdb798e85dfc9be2ce2f6de08b16a207523dcf
Chris Beeley <chris.beeley@gmail.com>
2021-07-08 16:52:19 +0100

    Only show date format box if data is loaded and user wants to format a date

--- R/mod_data_load.R
+++ R/mod_data_load.R
@@ -14,7 +14,8 @@ mod_data_load_ui <- function(id){
       column(
         width = 4,
         actionButton(ns("launch_modal"), "Upload new data"),
-        uiOutput(ns("date_picker"))
+        uiOutput(ns("format_checkbox")),
+        uiOutput(ns("date_picker")),
       ),
       column(
         width = 8,
@@ -53,12 +54,34 @@ mod_data_load_server <- function(id){
       imported$data()
     })
     
+    output$format_checkbox <- renderUI({
+      
+      req(imported$data())
+      
+      checkboxInput(session$ns("format_date"), "Format date?")
+    })
+    
     output$date_picker <- renderUI({
       
       req(imported$data())
       
-      selectInput("date_field", "Date column", 
-                  choices = names(imported$data()))
+      if(!input$format_date){
+        
+        return()
+      }
+      
+      tagList(
+        selectInput(session$ns("dateFormat"), "Date format",
+                    choices = c("DD-MM-YYYY" = "%d-%m-%Y", 
+                                "MM-DD-YYYY" = "%m-%d-%Y",
+                                "YYYY-MM-DD" = "%Y-%m-%d",
+                                "DD/MM/YYYY" = "%d/%m/%Y", 
+                                "MM/DD/YYYY" = "%m/%d/%Y",
+                                "YYYY/MM/DD" = "%Y/%m/%d")
+        ),
+        selectInput(session$ns("date_field"), "Date column", 
+                    choices = names(imported$data()))
+      )
     })
     
     reactive(
